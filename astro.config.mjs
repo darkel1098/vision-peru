82749657adebbe523856642d4034b42c9d7ee000
darkel18 <200~82195371+darkel1098@users.noreply.github.com>
2023-10-25 20:30:11 -0500

    update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: nodejs({
+    mode: 'middleware' // or 'standalone'
+  }),
   output: 'hybrid',
   site: "https://apps.lasolucionica.com",
   base: "visionperu",
